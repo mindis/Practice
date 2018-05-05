ae971ba3b3b7c3c443805de086944641762992e3
scheeloong <scheeloong@gmail.com>
2018-05-05 19:40:44 -0400

    Soon AutoCommit

--- Python/PyTorch/practice2.py
+++ Python/PyTorch/practice2.py
@@ -11,3 +11,4 @@ import torch
 
 # Extending torch.nn => Need set learnable parameters in init(), as well as forward() that calls an activation function that has backward() implemented
 # Also, each module can be an entire NN structure, instead of just a single function
+# Need to call parent constructor in extended child class to set up all the dictionaries and private membner variables of parent
