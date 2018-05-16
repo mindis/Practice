fe92b6a0b537ef67d4535e9212e02c9e5ccb74d6
scheeloong <scheeloong@gmail.com>
2018-05-16 01:47:48 -0400

    Doesnt compile, not sure if Maven configuration or Librec wasnt uploaded to maven repository to be able to do this

--- Java/Librec/Practice1/src/main/java/LibrecOne.java
+++ Java/Librec/Practice1/src/main/java/LibrecOne.java
@@ -3,6 +3,10 @@ package com.librecPracticeOne.app;
 
 import java.io.IOException;
 
+// Import LibRec Libraries
+// TODO: Test the import first to ensure compiling properly
+import net.librec.*;
+
 public class LibrecOne {
     private int temp; // useless dummy
 };
--- Java/Librec/Practice1/src/main/java/TestLibrecOne.java
+++ Java/Librec/Practice1/src/main/java/TestLibrecOne.java
@@ -1,12 +1,64 @@
 package com.librecPracticeOne.app;
-import java.io.IOException;
 import java.util.List; 
 import java.util.ArrayList;
+
 
 // public class TestZkConnection {
 public class TestLibrecOne {
     private static LibrecOne lrOne; 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws Exception {
         System.out.println("Testing librec One");
+
+        /*
+        // build data model
+        Configuration conf = new Configuration();
+        conf.set("dfs.data.dir", "G:/LibRec/librec/data");
+        TextDataModel dataModel = new TextDataModel(conf);
+        dataModel.buildDataModel();
+
+        // build recommender context
+        RecommenderContext context = new RecommenderContext(conf, dataModel);
+
+        // build similarity
+        conf.set("rec.recommender.similarity.key" ,"item");
+        RecommenderSimilarity similarity = new PCCSimilarity();
+        similarity.buildSimilarityMatrix(dataModel);
+        context.setSimilarity(similarity);
+
+        // build recommender
+        conf.set("rec.neighbors.knn.number", "5");
+        Recommender recommender = new ItemKNNRecommender();
+        recommender.setContext(context);
+
+        // run recommender algorithm
+        recommender.recommend(context);
+
+        // evaluate the recommended result
+        RecommenderEvaluator evaluator = new RMSEEvaluator();
+        System.out.println("RMSE:" + recommender.evaluate(evaluator));
+
+        // set id list of filter
+        List<String> userIdList = new ArrayList<>();
+        List<String> itemIdList = new ArrayList<>();
+        userIdList.add("1");
+        itemIdList.add("70");
+
+        // filter the recommended result
+        List<RecommendedItem> recommendedItemList = recommender.getRecommendedList();
+        GenericRecommendedFilter filter = new GenericRecommendedFilter();
+        filter.setUserIdList(userIdList);
+        filter.setItemIdList(itemIdList);
+        recommendedItemList = filter.filter(recommendedItemList);
+
+        // print filter result
+        for (RecommendedItem recommendedItem : recommendedItemList) {
+            System.out.println(
+                    "user:" + recommendedItem.getUserId() + " " +
+                    "item:" + recommendedItem.getItemId() + " " +
+                    "value:" + recommendedItem.getValue()
+            );
+        }
+        */
+        System.out.println("Done Testing");
     }
 }
