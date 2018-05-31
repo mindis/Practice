3ab38a83e1a44150c4d933239da06bdb218652a2
scheeloong <scheeloong@gmail.com>
2018-05-31 16:54:09 -0400

    Soon AutoCommit

--- Maven/debuggingWithMaven.sh
+++ Maven/debuggingWithMaven.sh
@@ -1,10 +1,13 @@
 #-------------------------------------------------------
 # Latest
 #-------------------------------------------------------
+Just refer to Practice/Java/Librec/PracticeTwo, it works
+#-------------------------------------------------------
+# Outdated 1
 # Update com.librecPracticeTwo.app.TestLibrecTwo with packageName.className
 mvn exec:exec -Dexec.executable="java" -Dexec.args="-classpath %classth -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=1044 com.librecPracticeTwo.app.TestLibrecTwo"
 #-------------------------------------------------------
-# Outdated
+# Outdated 2
 # On first terminal run
 # It will give you a port number, assume its 8000
 mvnDebug clean install # but this doesn't really detect anything from maven compilation, so not sure what this is for.
