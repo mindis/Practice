95088522705be1f67a5aa077b87fcb6315a8003a
scheeloong <scheeloong@gmail.com>
2018-05-31 13:33:37 -0400

    Soon AutoCommit

--- Maven/debuggingWithMaven.sh
+++ Maven/debuggingWithMaven.sh
@@ -1,6 +1,13 @@
+#-------------------------------------------------------
+# Latest
+#-------------------------------------------------------
+# Update com.librecPracticeTwo.app.TestLibrecTwo with packageName.className
+mvn exec:exec -Dexec.executable="java" -Dexec.args="-classpath %classth -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=1044 com.librecPracticeTwo.app.TestLibrecTwo"
+#-------------------------------------------------------
+# Outdated
 # On first terminal run
 # It will give you a port number, assume its 8000
-mvnDebug clean install
-
+mvnDebug clean install # but this doesn't really detect anything from maven compilation, so not sure what this is for.
 # Second terminal run
 jdb -attach localhost:8000
+#-------------------------------------------------------
